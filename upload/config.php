0a87c4222213b01fd6ad0680cf5e8552a2ba0d96
Maxim-tatasu <maksimplotnikov182@gmail.com>
2023-02-06 10:27:37 +0300

    Initial commit

---
+++ upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://store/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://store/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/store/upload/catalog/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/store/upload/system/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/store/upload/image/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/store/upload/catalog/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/store/upload/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/store/upload/system/config/');
+define('DIR_CACHE', 'C:/OpenServer/domains/store/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/store/upload/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/store/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/store/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/store/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'mystore');
+define('DB_PASSWORD', 'mystore');
+define('DB_DATABASE', 'mystore');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
